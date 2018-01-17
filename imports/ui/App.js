3d6b0fb962ec50a1d79a503a22553dafa15cef32
Brooks Forsyth <brooks.forsyth@gmail.com>
2018-01-16 21:56:02 -0500

    Finished step 7

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,22 +8,41 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
-    handleSubmit(event) {
-        event.preventDefault();
+  constructor(props) {
+    super(props);
 
-        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    this.state = {
+      hideCompleted: false,
+    };
+  }
 
-        Tasks.insert({
-            text,
-            createdAt: new Date(),
-        });
+  handleSubmit(event) {
+    event.preventDefault();
 
-        // Clear form
-        ReactDOM.findDOMNode(this.refs.textInput).value = '';
-    }
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
 
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -33,6 +52,16 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
